66ca4cf50f3976655a9d483220f317e369ebeac7
Maxime Thirouin <m@moox.io>
2015-05-12 07:42:31 +0200

    publish: Update url for "Les utilitaires en ligne de commande" + NodeJS => Node.js/iojs
    generated from commit 30bc331d985b770ddaead6e322edd713320b4da1

---
+++ tests.js
@@ -0,0 +1,87 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// webpack context helps to require a bundle of files
+	// dynamically, based on a regex
+	// here we bundle all test files like the following pattern
+	//
+	// ./(src|modules)/**/__tests__/*.js
+	//
+	"use strict";
+
+	var context = __webpack_require__(3);
+	context.keys().forEach(context);
+
+	// var contextComponents = require.context(
+	//   "./modules",
+	//   true, // **/*
+	//   /__tests__\/.*\.js$/
+	// )
+	// contextComponents.keys().forEach(contextComponents)
+
+/***/ },
+/* 2 */,
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	function webpackContext(req) {
+		throw new Error("Cannot find module '" + req + "'.");
+	}
+	webpackContext.keys = function() { return []; };
+	webpackContext.resolve = webpackContext;
+	module.exports = webpackContext;
+	webpackContext.id = 3;
+
+
+/***/ }
+/******/ ]);
